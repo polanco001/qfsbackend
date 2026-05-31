a1f48077c3dd94171015347fabbe04ce70b09a13
polanco001 <1776nationn@gmail.com>
2026-06-01 12:43:14 +1300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,8 +20,9 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: 'https://qfsledger-pyy7.onrender.com',
-  credentials: true
+  origin: ['https://qfsledger-pyy7.onrender.com', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }));
 
 app.use(express.json({ limit: '10kb' }));
